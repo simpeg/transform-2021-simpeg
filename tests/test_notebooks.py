deeac0ab844ca75481c43418d8aa42e857825fc1
Seogi Kang <sgkang09@gmail.com>
2021-04-18 17:29:19 -0700

    remove notebooks for testing

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -7,6 +7,12 @@ NBDIR = os.path.sep.join(
     os.path.abspath(__file__).split(os.path.sep)[:-2]
 )
 
+IGNORE = [
+    "1-magnetic-inversion-raglan-reproduce",
+    "3-magnetic-inversion-raglan-lp.ipynb",
+    "2-linear-inversion-app.ipynb"
+]
+
 Test = testipynb.TestNotebooks(directory=NBDIR, timeout=2800)
 TestNotebooks = Test.get_tests()
 
